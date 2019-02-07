0df3cf168d45f7987fca3a576ee193372c2e1868
DDoeDDoe32 <42180081+DDoeDDoe32@users.noreply.github.com>
2019-02-07 22:45:13 +0900

    Start(Test)

---
+++ Problem2839.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Problem2839
+{
+    public static void main(String[] args)
+    {
+        Scanner input1 = new Scanner(System.in);
+        
+        int number1 = input1.nextInt();
+
+        System.out.println("Hello world" + number1);
+
+        input1.close();
+    }
+}
